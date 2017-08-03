b8897df451294b674d5a04a95d9d3f20a5979ce5
彭硕 <pengshuo@pengshuodeMBP.lan>
2017-08-03 22:33:08 +0800

    bug session.userInfo

--- build/controllers/index.js
+++ build/controllers/index.js
@@ -1,35 +1,35 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 const Promise = require("bluebird");
 const request = Promise.promisifyAll(require("request"));
 exports.index = function (req, res, next) {
-    let session = req.session;
-    let daqizhixiang = request.getAsync('https://zhuanlan.zhihu.com/api/columns/DaqizhiXiang/posts?limit=20&offset=20');
-    let recommendations = request.getAsync('https://zhuanlan.zhihu.com/api/recommendations/posts?limit=5&offset=0&seed=4');
-    Promise
-        .all([daqizhixiang, recommendations])
-        .then((datalist) => {
-        let resData;
-        if (!datalist[0].error && datalist[0].statusCode == 200) {
-            let body = JSON.parse(datalist[0].body);
-            body.map(function (i) {
-                i.titleImage = "/fileProxy/image?url=" + i.titleImage;
-                return i;
-            });
-            resData = {
-                title: 'WhatEver',
-                articles: body,
-                recommendations: [{ title: "title1", url: "" }],
-                userInfo: session.userInfo
-            };
-        }
-        if (!datalist[1].error && datalist[1].statusCode == 200) {
-            let body = JSON.parse(datalist[1].body);
-            resData.recommendations = body;
-        }
-        return resData;
+  let session = req.session;
+  let daqizhixiang = request.getAsync('https://zhuanlan.zhihu.com/api/columns/DaqizhiXiang/posts?limit=20&offset=20');
+  let recommendations = request.getAsync('https://zhuanlan.zhihu.com/api/recommendations/posts?limit=5&offset=0&seed=4');
+  Promise
+    .all([daqizhixiang, recommendations])
+    .then((datalist) => {
+      let resData;
+      if (!datalist[0].error && datalist[0].statusCode == 200) {
+        let body = JSON.parse(datalist[0].body);
+        body.map(function (i) {
+          i.titleImage = "/fileProxy/image?url=" + i.titleImage;
+          return i;
+        });
+        resData = {
+          title: 'WhatEver',
+          articles: body,
+          recommendations: [{title: "title1", url: ""}],
+          userInfo: session && session.userInfo
+        };
+      }
+      if (!datalist[1].error && datalist[1].statusCode == 200) {
+        let body = JSON.parse(datalist[1].body);
+        resData.recommendations = body;
+      }
+      return resData;
     })
-        .then((resData) => {
-        res.render('index', resData);
+    .then((resData) => {
+      res.render('index', resData);
     });
 };
